13f976b25d1801f790c75e71bce060bc2bf98a79
Pascal Müller <MePs@viessmann.com>
2023-11-10 09:20:39 +0100

    basic setup to manage and render recipes

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,16 @@
-module.exports =  (config) => {
+const pluginSass = require('@grimlink/eleventy-plugin-sass');
+const sass = require('sass');
+const filterPostCss = require('./transform/post-css');
+
+module.exports = (config) => {
+
+    config.addPlugin(pluginSass, {sass});
+    config.addFilter('postCSS', filterPostCss);
+
+    config.addPassthroughCopy({
+        'src/assets/images/*.(jpg|png|webp)': 'img'
+    });
+
 
     return {
         dir: {
