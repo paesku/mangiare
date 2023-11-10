e0ee2ac979470fa08dc37bd77515ecaafee1ad3b
Pascal Müller <MePs@viessmann.com>
2023-11-10 13:50:51 +0100

    test css path

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,17 @@
+const { EleventyHtmlBasePlugin } = require('@11ty/eleventy');
 const pluginSass = require('@grimlink/eleventy-plugin-sass');
 const sass = require('sass');
 const filterPostCss = require('./transform/post-css');
 
 module.exports = (config) => {
 
+    config.addPlugin(EleventyHtmlBasePlugin);
     config.addPlugin(pluginSass, {sass});
     config.addFilter('postCSS', filterPostCss);
 
     config.addPassthroughCopy({
         'src/assets/images/*.(jpg|png|webp)': 'img'
     });
-
 
     return {
         dir: {
